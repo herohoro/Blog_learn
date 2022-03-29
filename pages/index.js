c100c99ce246b5235bbd55419b96d2c22d9911ac
へろほろ <mtwd.office.25@gmail.com>
2022-03-30 01:14:19 +0900

    test02

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
-
 export default function IndexPage() {
   return (
     <div>
       <h1>Hello World. </h1>
+      <p>csb-035iff ブランチ</p>
       <p>This page is Home</p>
       <Link href="/about">
         <a>👉　About page!</a>
