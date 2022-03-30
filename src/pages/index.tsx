b02163796516fb1c723e68cf79e83bc96a89dae9
へろほろ <mtwd.office.25@gmail.com>
2022-03-30 15:11:30 +0900

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,14 @@
-import Link from "next/link";
 import styles from "../styles/page.module.css";
 const RenderPage = () => (
   <div className={styles.container}>
     <div>
-      <h2>Welcome! </h2>
-      <p>branch 03_csb-nods6y</p>
-      <code>making like easy-notion-blog　</code>
-      <h3>This page is Home</h3>
-      <Link href="/about">
-        <a>👉　About page!</a>
-      </Link>
-      <br />
-      <Link href="/blog/first-page">
-        <a>👉　BlogList page!</a>
-      </Link>
+      <h2>Home page</h2>
+      <p>branch 05_csb-e9r66s</p>
+      <code>
+        making like easy-notion-blog
+        <br />
+        No component　
+      </code>
     </div>
   </div>
 );
