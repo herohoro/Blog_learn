5d703623802a0a1c28ee59a01234e64ea3ef1e61
へろほろ <mtwd.office.25@gmail.com>
2022-04-03 01:16:50 +0900

    initial commit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,17 @@ const AboutPage = () => {
   return (
     <div className={styles.container}>
       <h2>About page</h2>
-      <br />{" "}
+      <br />
+      <h3>API routing TEST</h3>
+      <form action="/api/form" method="post">
+        <label htmlFor="first">First name:</label>
+        <input type="text" id="first" name="first" />
+        <br />
+        <label htmlFor="last">Last name:</label>
+        <input type="text" id="last" name="last" />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
